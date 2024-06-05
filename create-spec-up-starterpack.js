25fe064e33b39b58b780358e6fa41f3c71b0b3af
kor <kor@dwarshuis.com>
2024-06-05 10:17:24 +0200

    Copy only starter-files dir

--- create-spec-up-starterpack.js
+++ create-spec-up-starterpack.js
@@ -6,38 +6,20 @@ const path = require('path');
 
 // Function to copy /spec directory, specs.json, and README.md to the target directory
 function setupSpecUpStarterPack(targetDir) {
-    const specDir = path.join(__dirname, 'spec');
-    const configFile = path.join(__dirname, 'specs.json');
-    const readmeFile = path.join(__dirname, 'README.md');
+    const starterFilesDir = path.join(__dirname, 'starter-files');
 
     if (!fs.existsSync(targetDir)) {
         fs.mkdirSync(targetDir, { recursive: true });
     }
 
     // Check if source files and directories exist
-    if (!fs.existsSync(specDir)) {
-        console.error(`Source directory ${specDir} does not exist.`);
+    if (!fs.existsSync(starterFilesDir)) {
+        console.error(`Source directory ${starterFilesDir} does not exist.`);
         process.exit(1);
     }
 
-    if (!fs.existsSync(configFile)) {
-        console.error(`Source file ${configFile} does not exist.`);
-        process.exit(1);
-    }
-
-    if (!fs.existsSync(readmeFile)) {
-        console.error(`Source file ${readmeFile} does not exist.`);
-        process.exit(1);
-    }
-
-    // Copy /spec directory
-    execSync(`cp -r ${specDir} ${targetDir}/spec`);
-
-    // Copy specs.json
-    fs.copyFileSync(configFile, path.join(targetDir, 'specs.json'));
-
-    // Copy README.md
-    fs.copyFileSync(readmeFile, path.join(targetDir, 'README.md'));
+    // Copy /starter-files directory
+    execSync(`cp -r ${starterFilesDir} ${targetDir}/`);
 
     console.log('Spec-up starterpack setup complete.');
 }
